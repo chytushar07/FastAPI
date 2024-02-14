354114c964ec0f9f6695b9a25a2d7bf7c6f637bb
chytushar07 <chytushar09@gmail.com>
2024-02-14 18:12:16 +0530

    added html file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post,user,auth,vote
 from .config import settings
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -21,6 +23,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Serve static files (like styles.css) from the 'static' directory
+app.mount("/static", StaticFiles(directory=os.path.join(current_dir, "static")), name="static")
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
@@ -29,9 +36,12 @@ app.include_router(vote.router)
 
 # Method used: GET and / defines url -All Methods[https://www.geeksforgeeks.org/different-kinds-of-http-requests/] where @ represents decorator 
 @app.get("/")
-async def root(): #Defining function//root is a function name
+async def root():
+    file_path = os.path.join(os.path.dirname(__file__), "documentation.html")
+    with open(file_path, "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
-    return {"Yello":"Successfull"}
 
 # @app.get('/sqlalchemy')
 # def test_posts(db: Session = Depends(get_db)): #getDB creates session everytime and generate sessions to it.
